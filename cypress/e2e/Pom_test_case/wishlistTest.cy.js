ae342fc6894c170c86df356866e6f06d04807290
अतुल <114137380+palatul16@users.noreply.github.com>
2025-02-09 21:33:59 +0530

    Wishlist page Update

--- cypress/e2e/Pom_test_case/wishlistTest.cy.js
+++ cypress/e2e/Pom_test_case/wishlistTest.cy.js
@@ -1,5 +1,6 @@
- import WishlistPage from '../../support/pageObjects/WishlistPage';
+import WishlistPage from '../../support/pageObjects/WishlistPage';
 import AdminPage from '../../support/pageObjects/AdminPage';
+
 
 const wishlistPage = new WishlistPage();
 const adminData = new AdminPage();
@@ -16,5 +17,24 @@ describe('Wishlist Test', () => {
     it('Add product to wishlist without login', () => {
         wishlistPage.toWishListPage();
     })
+
+    
+    it('Add product to wishlist with login', () => {
+        cy.wait(4000);
+        adminData.loginAdmin('xyzz@gmail.com', '123654');
+        wishlistPage.toWishListPagelogin();
+        
+    })
+
+     
+            // it('Checking the product is added to wishlist or not', () => {
+            //     cy.wait(4000);
+            //     adminData.loginAdmin('xyzz@gmail.com', '123654');
+            //     wishlistPage.tocheckwishlistProduct();
+                
+                
+            // })
+
+
     
 })
--- cypress/support/pageObjects/WishlistPage.js
+++ cypress/support/pageObjects/WishlistPage.js
@@ -2,13 +2,27 @@ class WishListPage{
     product  = "//div[normalize-space()='White']"
     wishlistLikeIcon = '.card-container > :nth-child(1) > .product-top-section > .product-icon > :nth-child(2) > .fa-regular'
     tostMessage = "#chakra-toast-manager-bottom"
-    wishlistIcon = "//p[@class='landing_page_header_fav']"
+    tostMessage1 = "#chakra-toast-manager-bottom"
+
+
+    wishlistIcon = (':nth-child(1) > .product-top-section > .product-icon > :nth-child(2) > .fa-solid');
     toWishListPage(){
         cy.xpath(this.product).click()
         cy.get(this.wishlistLikeIcon).click()
         cy.get(this.tostMessage).should('be.visible').and('contain.text', 'Login Required.')
-        
+        }
 
-    }
+    toWishListPagelogin(){
+        cy.xpath(this.product).click()
+        cy.get(this.wishlistLikeIcon).click()
+        cy.get(this.tostMessage1).should('be.visible').and('contain.text', 'Added to wishlist')
+        cy.get('.landing_page_header_fav > .svg-inline--fa > path').click();    
+        cy.get('.wishlist-header').should('be.visible').and('contain.text', 'Your WishList');
+
+        cy.get('.wishlist-card').should('be.visible');
+        cy.get('.remove-button').click();
+        }
+
+       
 }
 export default WishListPage;
