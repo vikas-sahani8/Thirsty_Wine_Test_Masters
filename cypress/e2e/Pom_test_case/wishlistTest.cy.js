3f966af58d788b6185a7c868cbe1264729bf4d31
Viki109 <sahani8vikas@gmail.com>
2025-02-09 20:44:19 +0530

    wishlistPage

--- cypress/e2e/Pom_test_case/wishlistTest.cy.js
+++ cypress/e2e/Pom_test_case/wishlistTest.cy.js
@@ -1,1 +1,20 @@
-import WishlistPage from '../../page-objects/WishlistPage';
+ import WishlistPage from '../../support/pageObjects/WishlistPage';
+import AdminPage from '../../support/pageObjects/AdminPage';
+
+const wishlistPage = new WishlistPage();
+const adminData = new AdminPage();
+
+const baseURL = "https://pay-pal-pioneers-068.vercel.app/"
+
+
+// ADD PRODUCT Into WISHLIST without login
+describe('Wishlist Test', () => {
+    beforeEach(() => {
+        cy.visit(baseURL);
+    })
+
+    it('Add product to wishlist without login', () => {
+        wishlistPage.toWishListPage();
+    })
+    
+})
--- cypress/support/pageObjects/WishlistPage.js
+++ cypress/support/pageObjects/WishlistPage.js
@@ -1,4 +1,14 @@
 class WishListPage{
+    product  = "//div[normalize-space()='White']"
+    wishlistLikeIcon = '.card-container > :nth-child(1) > .product-top-section > .product-icon > :nth-child(2) > .fa-regular'
+    tostMessage = "#chakra-toast-manager-bottom"
+    wishlistIcon = "//p[@class='landing_page_header_fav']"
+    toWishListPage(){
+        cy.xpath(this.product).click()
+        cy.get(this.wishlistLikeIcon).click()
+        cy.get(this.tostMessage).should('be.visible').and('contain.text', 'Login Required.')
+        
 
+    }
 }
 export default WishListPage;
