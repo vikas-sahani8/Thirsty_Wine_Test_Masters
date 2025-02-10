d6357da1ae731909afe924d706f26f0c68088b2c
अतुल <114137380+palatul16@users.noreply.github.com>
2025-02-10 08:36:56 +0530

    Product updated

--- cypress/e2e/Pom_test_case/wishlistTest.cy.js
+++ cypress/e2e/Pom_test_case/wishlistTest.cy.js
@@ -1,40 +1,33 @@
 import WishlistPage from '../../support/pageObjects/WishlistPage';
 import AdminPage from '../../support/pageObjects/AdminPage';
 
-
+// Creating instances of WishlistPage and AdminPage classes
 const wishlistPage = new WishlistPage();
 const adminData = new AdminPage();
 
-const baseURL = "https://pay-pal-pioneers-068.vercel.app/"
+// Base URL of the application
+const baseURL = "https://pay-pal-pioneers-068.vercel.app/";
 
+// Test suite for Wishlist functionality
+// This suite contains tests for adding products to the wishlist with and without login
 
-// ADD PRODUCT Into WISHLIST without login
 describe('Wishlist Test', () => {
+    // Before each test, visit the base URL
     beforeEach(() => {
         cy.visit(baseURL);
-    })
+    });
 
+    // Test case: Attempt to add a product to the wishlist without logging in
     it('Add product to wishlist without login', () => {
-        wishlistPage.toWishListPage();
-    })
+        wishlistPage.toWishListPage(); // Calls the method to add a product to the wishlist
+    });
 
-    
+    // Test case: Add a product to the wishlist after logging in
     it('Add product to wishlist with login', () => {
-        cy.wait(4000);
-        adminData.loginAdmin('xyzz@gmail.com', '123654');
-        wishlistPage.toWishListPagelogin();
-        
-    })
-
-     
-            // it('Checking the product is added to wishlist or not', () => {
-            //     cy.wait(4000);
-            //     adminData.loginAdmin('xyzz@gmail.com', '123654');
-            //     wishlistPage.tocheckwishlistProduct();
-                
-                
-            // })
-
+        cy.wait(4000); // Waits for 4 seconds before proceeding
+        adminData.loginAdmin('xyzz@gmail.com', '123654'); // Logs in using admin credentials
+        wishlistPage.toWishListPagelogin(); // Calls the method to add a product to the wishlist
+    });
 
     
-})
+});
--- cypress/support/pageObjects/ProductPage.js
+++ cypress/support/pageObjects/ProductPage.js
@@ -1,5 +1,18 @@
 class ProductPage{
 
+    gotoProductPage(){
+        cy.get('#landing_page_navbar > :nth-child(1)').click({ multiple: true });
+        cy.get('.card-container > :nth-child(1)').click();
+    }
+
+    CheckProduct(){
+        cy.get('.product-page-head > h1').should('be.visible').and('contain.text', 'Riesling Trocken, 0,75L');
+    }
+
+    instock(){
+        cy.get('.productpage-instock').should('be.visible').and('contain.text', 'In stock');
+    }
+
 }
 
 export default ProductPage;                       
